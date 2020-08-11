3a25f51988e888ec181d430c2f60d27b05a70a29
tkawakita <tomoya.kawakita110634@e-seikatsu.co.jp>
2020-08-11 23:30:02 +0900

    ヘッダーの作成・アイコンの追加
    どのページにもヘッダーが表示されるように、App.vueにヘッダーを追加。

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,13 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <img src="./assets/icon.png">
+    <div id="nav">
+      <router-link to="/">ホーム</router-link> |
+      <router-link to="/stampStore">スタンプ</router-link>
+      <router-link to="/pointStore">ポイント</router-link>
+      <router-link to="/login">ログイン</router-link>
+    </div>
+    <router-view />
   </div>
 </template>
 
@@ -12,6 +18,7 @@ export default {
 </script>
 
 <style>
+/* ヘッダーの表示を整える */
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
