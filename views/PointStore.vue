c447fd762bec5a125a95e160c6ab4a377a10ac1b
tkawakita <tomoya.kawakita110634@e-seikatsu.co.jp>
2020-08-11 17:27:43 +0900

    PointStore.vue（ポイント購入画面）の作成
    全体の大枠を作成。CSSは未完成

---
+++ views/PointStore.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="pointStore">
+        <!-- 商品（＝ポイント）一覧の表示 -->
+        <div v-for="point in points" v-bind:key="point">
+            <PointChoice :point="point"></PointChoice>
+        </div>
+    </div>
+</template>
+
+<script>
+import PointChoice from "../components/PointChoice";
+
+export default {
+    name: "PointStore",
+    data: () => {
+        return {
+            points: [100, 1000, 5000] // ポイント（価格）の種類
+        };
+    },
+    components: {
+        PointChoice
+    }
+}
+</script>
