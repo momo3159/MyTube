67456b65ed84e47342e19dd1cf0654c181018b66
tkawakita <tomoya.kawakita110634@e-seikatsu.co.jp>
2020-08-12 00:20:36 +0900

    スタンプ購入画面の作成
    スタンプの一覧を表示。今はスタンプの情報はコンポーネントに記載。

---
+++ views/StampStore.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="stampStore">
+        <h1>スタンプストア</h1>
+        <!-- 検索 title, authorで判定すれば良い-->
+        <!-- TODO: ポイントが足りない時の処理 -->
+        <!-- ポイントはサーバーから取得（？） -->
+        <div class="stamp" v-for="stamp in stamps" v-bind:key="stamp.title">
+            <img v-bind:src="stamp.image">
+            <div class="author">{{stamp.author}}</div>
+            <div class="title">{{stamp.title}}</div>
+            <div class="price">{{stamp.point}}</div>
+            <input type="button" value="購入する" v-on:click="buy(stamp)">
+        </div>    
+    </div>
+    
+</template>
+
+<script>
+export default {
+    name: "StampStore",
+    data: ()=> {
+        return {
+            // 本来はサーバーサイドから取ってくるべきデータ（？）
+            stamps: [
+                {author: "hoge", title: "hoge", point: 100, image: "../assets/"},
+                {author: "piyo", title: "piyo", point: 50, image: "../assets/"},
+                {author: "hogehoge", title: "hogehoge", point: 200, image: "../assets/"},
+                {author: "foo", title: "foo", point: 50, image: "../assets/"},
+                {author: "bar", title: "bar", point: 100, image: "../assets/"},
+            ]
+        }
+    },
+    methods: {
+        buy: stamp =>{
+            // デバッグ用
+            console.log(`${stamp.author}の${stamp.title}を購入しました`);
+            console.log(`${stamp.point}ポイントを使いました`);
+        }
+    }
+}
+</script>
+
+<style scoped>
+    /* TODO: スタンプの表示の仕方を整える*/
+</style>
