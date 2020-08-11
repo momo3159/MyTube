bbb15c87dc1c002975e7735e2a2368be1cac6cb3
tkawakita <tomoya.kawakita110634@e-seikatsu.co.jp>
2020-08-12 00:34:41 +0900

    Revert "ルートの追加"
    This reverts commit 0384511913ceea58d29bd49b8134a856d943f640.

--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,15 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import HelloWorld from '@/components/HelloWorld'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
-    // ここにHOMEのルート
     {
-      path: "/stampStore",
-      name: "StampStore",
-      component: () =>
-        import("../views/StampStore.vue")
-    },
-    {
-      path: "/pointStore",
-      name: "PointStore",
-      component: () => 
-        import("../views/PointStore.vue")
+      path: '/',
+      name: 'HelloWorld',
+      component: HelloWorld
     }
   ]
 })
