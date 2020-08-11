428143472a7cab515e64c0a94c383e21573e9ae0
tkawakita <tomoya.kawakita110634@e-seikatsu.co.jp>
2020-08-11 16:55:12 +0900

    PointChoice.vueの作成。 ポイント購入画面におけるポイント購入の一つの商品コンポーネントを表す。

---
+++ src/components/PointChoice.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        {{point}}ポイント
+        <!-- TODO: v-onで購入処理を追加 -->
+        <input type="button" value="購入">
+    </div>
+</template>
+
+<script>
+export default {
+    name: "PointChoice",
+    props: {
+        point: Number
+    },
+    methods: {
+        // TODO: ボタンクリック時のイベントを追加
+    }
+}
+</script>
