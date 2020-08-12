c98dafc14d36e0af709394d0e0af195043c986a4
tkawakita <tomoya.kawakita110634@e-seikatsu.co.jp>
2020-08-12 18:20:06 +0900

    ヘッダーのCSSを追加
    CSSの記述のために、ヘッダーのHTMLにdivタグを追加した。

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,18 @@
 <template>
   <div id="app">
-    <img src="./assets/icon.png">
-    <div id="nav">
-      <router-link to="/">ホーム</router-link> |
-      <router-link to="/stampStore">スタンプ</router-link>
-      <router-link to="/pointStore">ポイント</router-link>
-      <router-link to="/login">ログイン</router-link>
-    </div>
+    <header>
+      <div id="nav">
+        <div class="header-left">
+          <img id="header-icon" src="./assets/icon.png">
+        </div>
+        <div class="header-right">
+          <router-link to="/">ホーム</router-link> |
+          <router-link to="/StampStore">スタンプ</router-link> | 
+          <router-link to="/PointStore">ポイント</router-link> |
+          <router-link to="/login">ログイン</router-link>
+        </div>
+      </div>
+    </header>
     <router-view />
   </div>
 </template>
@@ -18,13 +24,47 @@ export default {
 </script>
 
 <style>
-/* ヘッダーの表示を整える */
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
+
+header {
+  height: 65px;
+  width: 100%;
+  background-color: black;
+  position: fixed;
+  top: 0;
+  z-index: 10;
+}
+#header-icon {
+  width: 150px;
+  margin-top: 15px;
+}
+
+#nav {
+  width: 1170px;
+  padding: 0 15px;
+  margin: 0 auto;
+}
+.header-left {
+  float: left;
+}
+.header-right {
+  float: right;
+}
+
+.header-right a{
+  line-height: 65px;
+  padding: 0 25px;
+  color: white;
+}
+
+#nav a.router-link-exact-active {
+  color: orange;
+}
+
 </style>
