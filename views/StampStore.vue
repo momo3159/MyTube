74b95d1e3037827fe373561cdda1fc4ac50c93f7
tkawakita <tomoya.kawakita110634@e-seikatsu.co.jp>
2020-08-12 18:12:14 +0900

    スタンプのデータの変更
    スタンプの種類を追加した。また、画像のパスをそのまま記述すると画像が読み込まれないため、画像をモジュールとして読み込むためにrequire()を用いた。

--- views/StampStore.vue
+++ views/StampStore.vue
@@ -22,11 +22,14 @@ export default {
         return {
             // 本来はサーバーサイドから取ってくるべきデータ（？）
             stamps: [
-                {author: "hoge", title: "hoge", point: 100, image: "../assets/"},
-                {author: "piyo", title: "piyo", point: 50, image: "../assets/"},
-                {author: "hogehoge", title: "hogehoge", point: 200, image: "../assets/"},
-                {author: "foo", title: "foo", point: 50, image: "../assets/"},
-                {author: "bar", title: "bar", point: 100, image: "../assets/"},
+                {author: "hoge", title: "ハムスターセット", point: 100, image: require("../assets/hamster_sleep.png")},
+                {author: "piyo", title: "やらかした人シリーズ", point: 50, image: require("../assets/coffee_kobosu.png")},
+                {author: "hogehoge", title: "棒人間シリーズ", point: 200, image: require("../assets/figure_sleeping.png")},
+                {author: "foo", title: "小学生", point: 50, image: require("../assets/mizu_boy.png")},
+                {author: "bar", title: "オリンピック", point: 100, image: require("../assets/parade.png")},
+                {author: "bar", title: "動物シリーズ", point: 100, image: require("../assets/penguin.png")},
+                {author: "bar", title: "花見シリーズ", point: 100, image: require("../assets/ukareru_man.png")},
+                {author: "bar", title: "Vue.js", point: 100, image: require("../assets/logo.png")}
             ]
         }
     },
