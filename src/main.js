1c6f8b22841f6c55154671ec28652caf95ef9470
mercury-pluto1210 <mercury.pluto1210@gmail.com>
2020-08-13 11:21:52 +0900

    ホーム画面の作成

--- src/main.js
+++ src/main.js
@@ -3,7 +3,13 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
+library.add(fas)
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
